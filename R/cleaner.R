6385919d580e28f915d2065961fe44a5582a72b9
JL Paniangvait <phimmada@berkeley.edu>
2025-11-13 13:55:55 -0800

    initial commit

---
+++ R/cleaner.R
@@ -0,0 +1,13 @@
+#' Clean Vairables
+#' 
+#' @param citbi A dataset.
+#' @returns A dataset cleaned for missing values.
+cleaner <- function(citbi) {
+  citbi <- citbi |>
+    mutate(LocLen = ifelse(LocLen == 92, NA, LocLen), 
+      SeizLen = ifelse(SeizLen == 92, NA, SeizLen))
+      citbi <- citbi |>
+    mutate(Amnesia_verb = ifelse(Amnesia_verb == 91, NA, Amnesia_verb),
+      HA_verb = ifelse(HA_verb == 91, NA, HA_verb))
+  return(citbi)
+}
---
+++ R/namer.R
@@ -0,0 +1,17 @@
+#' Rename Variables
+#' 
+#' @param citbi A dataset.
+#' @returns A dataset with cleaned names.
+namer <- function(citbi) {
+  citbi <- citbi |>
+    rename(patient_number = PatNum, amnesia = Amnesia_verb, loss_of_consciousness_length = LocLen,
+      seizure = Seiz, seizure_length = SeizLen, acting_normal = ActNorm,
+      headache = HA_verb, vomiting = Vomit, dizziness = Dizzy,
+      gcs_eye = GCSEye, gcs_verbal = GCSVerbal, gcs_motor = GCSMotor,
+      gcs_total = GCSTotal, altered_mental_status = AMS, skull_fracture = SFxPalp,
+      fontanelle_bulging = FontBulg, hematoma = Hema, clavicle_trauma = Clav,
+      neurological_deficit = NeuroD, other_significant_injury = OSI, ct_form = CTForm1,
+      age_in_month = AgeInMonth, gender = Gender, ct_done = CTDone,
+      death_tbi = DeathTBI, citbi_outcome = PosIntFinal)
+  return(citbi)
+}
