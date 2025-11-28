d0d1fa1b8ae581dec9ab77da2b188be0c0a0da9e
JL Paniangvait <phimmada@berkeley.edu>
2025-11-27 22:32:05 -0500

    Adding dataset

--- R/citbi.R
+++ R/citbi.R
@@ -2,6 +2,7 @@
 #' 
 #' Dataset on brain trauma patients
 #' 
+#' @format A dataset recording a patient on each row and each of their test results in each column.
+#' 
 #' @source STAT 133
-
 "citbi"
